2d1291c3c623a507c3dd21c272d55c4a085c115d
kolya-sp <63204488+kolya-sp@users.noreply.github.com>
2026-06-12 01:36:19 +0300

    make TcpListener nullable

--- EchoTcpServer/Program.cs
+++ EchoTcpServer/Program.cs
@@ -14,7 +14,7 @@ namespace EchoServer;
 public class EchoServer
 {
     private readonly int _port;
-    private TcpListener _listener;
+    private TcpListener? _listener;
     private readonly CancellationTokenSource _cancellationTokenSource;
 
 
